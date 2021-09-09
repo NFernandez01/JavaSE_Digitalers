e31d39f51a70fae4397fafa00d554e41b3b2857b
Nahuel Fernandez <fernandeznahuel408@gmail.com>
2021-09-09 15:45:44 -0300

    Creacion de entidad Alumno en DAO

---
+++ DAO/src/main/java/com/educacionit/entidades/Alumno.java
@@ -0,0 +1,60 @@
+package com.educacionit.entidades;
+
+import java.util.Date;
+
+public class Alumno {
+	private Documento documento;
+	private String descripcion;
+	private Date fechaNacimiento;
+	private boolean activo;
+	
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Alumno(Documento documento, String descripcion, Date fechaNacimiento, boolean activo) {
+		super();
+		this.documento = documento;
+		this.descripcion = descripcion;
+		this.fechaNacimiento = fechaNacimiento;
+		this.activo = activo;
+	}
+
+	public Documento getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(Documento documento) {
+		this.documento = documento;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [ " + documento + ", " + descripcion + ", "
+				+ fechaNacimiento + ", " + activo + "]";
+	}
+}
